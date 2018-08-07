636ba39b94598adbdc7b2cddfade949dadfac6da
shelps <italoguilherme115@hotmail.com>
2018-08-07 14:20:44 -0300

    detail page

--- src/pages/PeopleDetailPage.tsx
+++ src/pages/PeopleDetailPage.tsx
@@ -9,12 +9,19 @@ export default class PeopleDetail extends React.Component<NavigationScreenProps,
   render() {
 
     const { people }:any = this.props.navigation.state.params;
+    
     return (
       <View style={styles.container}>
          <Image 
             source={{uri: people.picture.large}}
             style={styles.avatar}>
          </Image>
+         <View style={styles.detailContainer}>
+          <View style={styles.line}>
+            <Text style={[styles.cell, styles.cellLabel]} >Email:</Text>
+            <Text style={[styles.cell, styles.cellContent]} >{people.email}</Text>
+          </View>
+         </View>
       </View>
     );
   }
@@ -26,5 +33,27 @@ const styles = StyleSheet.create({
     },
     avatar: {
         aspectRatio: 1,
+    },
+    detailContainer: {
+      backgroundColor: '#e2f9ff',
+      marginTop: 20,
+      elevation: 1
+    }, 
+    line: {
+      flexDirection: 'row',
+      paddingTop: 3,
+      paddingBottom: 3,
+      borderWidth: 1,
+      borderColor: "#C5C5C5",
+    }, 
+    cell: {
+      fontSize: 18,
+      paddingLeft: 5,
+    },
+    cellLabel: {
+      fontWeight: "bold" 
+    },
+    cellContent: {
+      fontStyle: 'italic',
     }
 })
